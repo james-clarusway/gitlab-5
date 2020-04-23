c79e7261c9d4445b1e86eca4843942304c828a4c
Ramazan E <ergunrkyrd@gmail.com>
2020-04-23 13:54:51 +0300

    aaa

--- a.py
+++ a.py
@@ -1,7 +1,3 @@
-name = input('Please Enter Your First name:\n')
-passwd = {'Mark' :'Tr678&.H',}
-first = name.title()
-if first in passwd:
-  print("Hello,",first,"!", "The password is : ",passwd[first])
-else:
-  print("Hello,",first,"\b!", "See you later.")
+a = 5
+
+print(str(a))
